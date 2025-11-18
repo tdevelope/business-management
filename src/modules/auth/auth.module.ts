0ab018ae5b247e5a4eeb44d6c85f9bcbfaa4e5c0
tdevelope <tmr.develope@gmail.com>
2025-11-18 14:52:19 +0200

    feat(auth): add basic register & login with JWT

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
-@Module({})
+@Module({
+    imports: [
+    JwtModule.register({
+        secret: 'SUPER_SECRET_KEY',
+        signOptions: { expiresIn: '3h' },
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
+})
 export class AuthModule {}
---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class AuthService {
+  private prisma = new PrismaClient();
+
+    constructor(private jwtService: JwtService) {}
+
+    private generateToken(user: { id: number; email: string; role: string }) {
+        const payload = {
+            sub: user.id,
+            email: user.email,
+            role: user.role,
+        };
+
+        const token = this.jwtService.sign(payload);
+        return { token };
+    }
+
+
+    async register(dto: { name: string; email: string; password: string; role: string }) {
+        const existing = await this.prisma.user.findUnique({
+            where: { email: dto.email },
+        });
+
+        if (existing) {
+            throw new Error('User with this email already exists');
+        }
+
+        const hashedPassword = await bcrypt.hash(dto.password, 10);
+
+        const user = await this.prisma.user.create({
+            data: {
+            name: dto.name,
+            email: dto.email,
+            password: hashedPassword,
+            role: dto.role || 'customer',
+            },
+        });
+
+        return this.generateToken(user);
+    }
+
+    async login(dto: { email: string; password: string }) {
+        const user = await this.prisma.user.findUnique({
+            where: { email: dto.email },
+        });
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        const isValid = await bcrypt.compare(dto.password, user.password);
+
+        if (!isValid) {
+            throw new Error('Invalid password');
+        }
+        
+        return this.generateToken(user);
+    }
+
+}
