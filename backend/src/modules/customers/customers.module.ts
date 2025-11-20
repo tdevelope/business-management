e4663e88f090c7c021a6c8430d051971de181bc5
tdevelope <tmr.develope@gmail.com>
2025-11-20 17:31:06 +0200

    added customer logic

--- backend/src/modules/customers/customers.module.ts
+++ backend/src/modules/customers/customers.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { CustomersService } from './customers.services';
+import { CustomersController } from './customers.controller';
 
-@Module({})
+@Module({
+  imports: [PrismaModule],
+  providers: [CustomersService],
+  controllers: [CustomersController],
+})
 export class CustomersModule {}
