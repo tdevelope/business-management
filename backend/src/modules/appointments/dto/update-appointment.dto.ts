498d9143ee00f039fdada226c05f4a0317aebd46
tdevelope <tmr.develope@gmail.com>
2025-12-02 12:39:44 +0200

    feat: add full appointment editing flow for admin and customer

---
+++ backend/src/modules/appointments/dto/update-appointment.dto.ts
@@ -0,0 +1,17 @@
+import { IsOptional, IsString } from "class-validator";
+
+export class UpdateAppointmentDto {
+  @IsOptional()
+  @IsString()
+  date?: string;
+
+  @IsOptional()
+  @IsString()
+  preferredTime?: string;
+
+  @IsOptional()
+  startTime?: string | Date;
+
+  @IsOptional()
+  endTime?: string | Date;
+}
