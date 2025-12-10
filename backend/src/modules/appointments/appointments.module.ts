5d6d217bfe94d626f91ff158eaec44739997abb9
tdevelope <tmr.develope@gmail.com>
2025-12-10 13:34:28 +0200

    fix: imports in modules

--- backend/src/modules/appointments/appointments.module.ts
+++ backend/src/modules/appointments/appointments.module.ts
@@ -5,9 +5,11 @@ import { AppointmentsController } from './appointments.controller';
 import { BusinessSettingsModule } from '../business-settings/business-settings.module';
 import { WaitlistModule } from '../waitlist/waitlist.module';
 import { WaitlistService } from '../waitlist/waitlist.service';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-    imports: [PrismaModule, BusinessSettingsModule, WaitlistModule],
+    imports: [PrismaModule, BusinessSettingsModule, WaitlistModule, NotificationsModule, UsersModule],
     providers: [AppointmentsService, WaitlistService],
     controllers: [AppointmentsController]
 })
