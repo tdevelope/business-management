e8c60c167f4b1a1508f81c9274b8823bc8486f99
tdevelope <tmr.develope@gmail.com>
2025-12-14 14:18:55 +0200

    replaced icon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/icon-dark-32x32.png",
         media: "(prefers-color-scheme: light)",
       },
       {
@@ -22,11 +22,11 @@ export const metadata: Metadata = {
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/icon-dark-32x32.png",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/icon-dark-32x32.png",
   },
 }
 
